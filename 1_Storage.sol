723d28b31baa648c3fedbb199b363f97b7ff5117
ANDRE BADENAS DOS SANTOS <48100164+badenas1@users.noreply.github.com>
2023-08-10 17:53:09 -0300

    Update 1_Storage.sol
    add code to the file

--- 1_Storage.sol
+++ 1_Storage.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract testBaseDeploy {
+
+uint256 private counter;
+
+event CounterUpdated(uint256 newValue);
+
+constructor() {
+
+counter = 0;
+
+}
+
+function getCounter() public view returns (uint256) {
+
+return counter;
+
+}
+
+function increment() public {
+
+counter++;
+
+emit CounterUpdated(counter);
+
+}
+
+}
